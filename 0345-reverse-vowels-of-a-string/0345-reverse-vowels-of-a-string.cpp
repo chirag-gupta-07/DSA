3c7ea445d6237ee3498efa6f475fa2e6b497e053
Chirag Gupta <chiraggupta9760@gmail.com>
2026-08-14 17:47:48 +0530

    Time: 0 ms (100%), Space: 10.2 MB (92.53%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        int i = 0;
-        int j = s.size() - 1;
-
-        while (i < j) {
-
-            while (i < j &&
-                   s[i] != 'a' && s[i] != 'e' && s[i] != 'i' &&
-                   s[i] != 'o' && s[i] != 'u' &&
-                   s[i] != 'A' && s[i] != 'E' && s[i] != 'I' &&
-                   s[i] != 'O' && s[i] != 'U') {
+        int i=0;
+        int j=s.size()-1;
+        if(s.size()==0){
+            return s;
+        }
+        while(i<j){
+            while(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' && s[i]!='A' && s[i]!='E' && s[i]!='I' && s[i]!='O' && s[i]!='U' && i<j){
                 i++;
             }
-
-            while (i < j &&
-                   s[j] != 'a' && s[j] != 'e' && s[j] != 'i' &&
-                   s[j] != 'o' && s[j] != 'u' &&
-                   s[j] != 'A' && s[j] != 'E' && s[j] != 'I' &&
-                   s[j] != 'O' && s[j] != 'U') {
+            while(s[j]!='A' && s[j]!='E' && s[j]!='I' && s[j]!='O' && s[j]!='U' && s[j]!='a' && s[j]!='e' && s[j]!='i' && s[j]!='o' && s[j]!='u' && i<j){
                 j--;
             }
-
-            swap(s[i], s[j]);
-
+            char c=s[i];
+            s[i]=s[j];
+            s[j]=c;
             i++;
             j--;
         }
