c26a2e5ac876bb617b97db01d05a19d639e2373b
Chirag Gupta <chiraggupta9760@gmail.com>
2026-08-28 16:26:22 +0530

    Time: 19 ms (10.96%), Space: 60.1 MB (5.18%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int carry=0;
+        int sum=0;
+        string ans="";
+        int i=0;
+        while(i<num1.size() && i<num2.size()){
+            int a=(num1[num1.size()-1-i])-(int)'0';
+            int b=num2[num2.size()-1-i]-(int)'0';
+            sum=a+b+carry;
+            carry=sum/10;
+            ans=to_string(sum%10)+ans;
+            i++;
+        }
+
+        while(i<num1.size()){
+            int a=num1[num1.size()-1-i]-(int)'0';
+            sum=a+carry;
+            carry=sum/10;
+            ans=to_string(sum%10)+ans;
+            i++;
+        }
+
+        while(i<num2.size()){
+            int b=num2[num2.size()-1-i]-(int)'0';
+            sum=b+carry;
+            carry=sum/10;
+            ans=to_string(sum%10)+ans;
+            i++;
+        }
+
+        if(carry){
+            ans=to_string(carry)+ans;
+        }
+
+        return ans;
+    }
+};
